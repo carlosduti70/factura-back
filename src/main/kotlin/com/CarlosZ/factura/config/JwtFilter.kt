7a9ae2c6aa7abef4cdfe17cb2715b599ac443f32
carlosduti70 <cmzuniga@sudamericano.edu.ec>
2024-01-15 10:16:21 -0500

    imports

--- src/main/kotlin/com/CarlosZ/factura/config/JwtFilter.kt
+++ src/main/kotlin/com/CarlosZ/factura/config/JwtFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
@@ -11,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.http.HttpHeaders
 
 @Component
 class JwtFilter: OncePerRequestFilter() {
